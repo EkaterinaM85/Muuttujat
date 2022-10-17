f5c590ad3c319b9febf7bbdd1fbdc97ea518ee54
EkaterinaM85 <mangka22@rpkk.fi>
2022-10-17 14:24:14 +0300

    first commit

---
+++ muuttujat.js
@@ -0,0 +1,40 @@
+//Muuttujat
+//tehtävä 1
+d = 2;
+a = 5;
+b = d+a;
+console.log(b);
+//tehtävä 2
+e = 3;
+e =e+1;
+e =e+1;
+e =e+1;
+console.log(e);
+//tehtävä 3
+d=1;
+e=3;
+d=d+e;
+e=d+e;
+d=d+e;
+console.log(d);
+//tehtävä 4
+d=3;
+b=2;
+e=d+2*b;
+c=d+b+e;
+console.log(c);
+//tehtävä 5
+c=1;
+b=5;
+d=c;
+c=b;
+b=d;
+console.log(b);
+//tehtävä 6
+d=2;
+d=d*d;
+d=d*d;
+d=d*d;
+d=d*d;
+console.log(d);
+
